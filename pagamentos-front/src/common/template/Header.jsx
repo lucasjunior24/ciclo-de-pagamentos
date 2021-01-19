ff3391ba886b032487cc92502bba02b80c5f917f
lucasjunior24 <lukateku@gmail.com>
2021-01-19 16:50:07 -0300

    Criando Header

--- pagamentos-front/src/common/template/Header.jsx
+++ pagamentos-front/src/common/template/Header.jsx
@@ -4,10 +4,10 @@ export default function Header() {
     return (
         <header className='main-header'>
             <a href='/#/' className='logo'>
-                <span className='logo-mini'><b>My</b>M</span>
+                <span className='logo-mini'><b>My</b> M</span>
                 <span className='logo-lg'>
                     <i className='fa fa-money'></i>
-                    <b>My</b>Money
+                    <b>My</b> Money
                 </span>
             </a>
             <nav className='navbar navbar-static-top'>
