5236b682e02656cd37773870cdbcbf4c7c84caf0
lucasjunior24 <lukateku@gmail.com>
2021-01-19 16:48:13 -0300

    Definindo layout

---
+++ pagamentos-front/src/main/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import '../common/template/dependencias'
+
+import Header from '../common/template/Header'
+
+function App() {
+  return (
+    <div className="wrapper">
+      <Header />
+    </div>
+  );
+}
+
+export default App;
