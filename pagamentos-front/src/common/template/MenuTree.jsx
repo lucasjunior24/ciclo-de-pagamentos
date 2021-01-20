957a02b5a60ebc39a00ff41ec8ca1e1287554faf
lucasjunior24 <lukateku@gmail.com>
2021-01-20 16:03:21 -0300

    Finalizando Menu

--- pagamentos-front/src/common/template/MenuTree.jsx
+++ pagamentos-front/src/common/template/MenuTree.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 
 export default props => (
-    <li>
-        <a href={props.path}>
+    <li className='treeview'>
+        <a>
             <i className={`fa fa-${props.icon}`}></i>{props.label}
+            <i className='fa fa-angle-left pull-right'></i>
         </a>
+        <ul className='treeview-menu'>
+            {props.children}
+        </ul>
     </li>
 )
 
