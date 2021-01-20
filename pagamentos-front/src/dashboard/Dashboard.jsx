c40b8ab8e4802a0db1e531466f9b116629e99d95
lucasjunior24 <lukateku@gmail.com>
2021-01-20 18:43:57 -0300

    Configurando servidor

--- pagamentos-front/src/dashboard/Dashboard.jsx
+++ pagamentos-front/src/dashboard/Dashboard.jsx
@@ -1,10 +1,16 @@
-import React from 'react';
+import React, { Component }  from 'react';
 
-export default function Dashboard() {
-    return (
-        <div>
-            <h1>Dashboard</h1>
-        </div>
-    )
+import Content from '../common/template/Content';
+import ContentHeader from '../common/template/ContentHeader';
+
+export default class Dashboard extends Component {
+    render() {
+        return (
+            <div>
+                <ContentHeader title='Dashboard' small='Versão 1.0' />
+                <Content />
+            </div>
+        )
+    }    
 }
 
