73f95b85101a71c29df319b88326f2a803b4166a
lucasjunior24 <lukateku@gmail.com>
2021-01-23 17:00:34 -0300

    Criando componente ValueBox

--- pagamentos-front/src/dashboard/Dashboard.jsx
+++ pagamentos-front/src/dashboard/Dashboard.jsx
@@ -2,13 +2,24 @@ import React, { Component }  from 'react';
 
 import Content from '../common/template/Content';
 import ContentHeader from '../common/template/ContentHeader';
+import ValueBox from '../common/widget/ValueBox';
+import Row from '../common/layout/Row'
 
 export default class Dashboard extends Component {
     render() {
         return (
             <div>
                 <ContentHeader title='Dashboard' small='Versão 1.0' />
-                <Content />
+                <Content>
+                    <Row>
+                        <ValueBox cols='12 4' color='green' icon='bank'
+                            value='R$ 10' text='Total de Créditos' />
+                        <ValueBox cols='12 4' color='red' icon='credit-card'
+                            value='R$ 10' text='Total de Debitos' />
+                        <ValueBox cols='12 4' color='blue' icon='money'
+                            value='R$ 0' text='Valor consolidado' />
+                    </Row>
+                </Content>
             </div>
         )
     }    
