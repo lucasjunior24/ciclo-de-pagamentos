a89e37e8127648e14546c1c25aba26b16c209451
lucasjunior24 <lukateku@gmail.com>
2021-01-28 18:26:50 -0300

    Criando componentes da pasta tab

--- pagamentos-front/src/billingCycle/BillingCycle.jsx
+++ pagamentos-front/src/billingCycle/BillingCycle.jsx
@@ -1,11 +1,37 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-function BillingCycle(props) {
-    return (
-        <div>
-            <h1>Ciclo de Pagamento</h1>
-        </div>
-    )
+import ContentHeader from '../common/template/ContentHeader'
+import Content from '../common/template/Content'
+import Tabs from '../common/tab/Tabs';
+import TabsHeader from '../common/tab/TabsHeader';
+import TabHeader from '../common/tab/TabHeader';
+import TabsContent from '../common/tab/TabsContent'
+import TabContent from '../common/tab/TabContent';
+
+class BillingCycle extends Component {
+    render() {
+        return (
+            <div>
+                <ContentHeader title='Ciclos de Pagamentos' small='Cadastro' />
+                <Content>
+                    <Tabs>
+                        <TabsHeader>
+                            <TabHeader label='Listar' icon='bars' target='tabList' />
+                            <TabHeader label='Incluir' icon='plus' target='tabCreate' />
+                            <TabHeader label='Alterar' icon='pencil' target='tabUpdate' />
+                            <TabHeader label='Excluir' icon='trash-0' target='tabDelete' />
+                        </TabsHeader>
+                        <TabsContent>
+                            <TabContent id='tabList'><h1>Lista</h1></TabContent>
+                            <TabContent id='tabCreate'><h1>Incluir</h1></TabContent>
+                            <TabContent id='tabUpdate'><h1>Alterar</h1></TabContent>
+                            <TabContent id='tabDelete'><h1>Excluir</h1></TabContent>
+                        </TabsContent>
+                    </Tabs>
+                </Content>
+            </div>
+        )
+    }
 }
 
 export default BillingCycle
