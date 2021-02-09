59e646c8c047b25b55ebc11176888d500cf2bc14
lucasjunior24 <lukateku@gmail.com>
2021-02-09 11:46:15 -0300

    Melhorando layout do formulario

---
+++ pagamentos-front/src/common/form/labelAndInput.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Grid from '../layout/Grid'
+
+export default function(props) {
+    return (
+        <Grid cols={props.cols}>
+            <div className='form-group'>
+                <label htmlFor={props.name}>{props.label}</label>
+                <input {...props.input} 
+                    className='form-control'
+                    placeholder={props.placeholder}
+                    readOnly={props.readOnly} 
+                    type={props.type} />
+            </div>
+        </Grid>
+    )
+}
