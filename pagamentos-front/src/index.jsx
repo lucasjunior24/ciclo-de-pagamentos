f914e03a6618574c9b5f7f3ff440b49160fef370
lucasjunior24 <lukateku@gmail.com>
2021-02-09 11:30:09 -0300

    Melhando inclusao no formulario

--- pagamentos-front/src/index.jsx
+++ pagamentos-front/src/index.jsx
@@ -6,10 +6,13 @@ import { Provider } from 'react-redux'
 
 import reducers from './main/reducers'
 import promise from 'redux-promise'
+import multi from 'redux-multi'
+import thunk from 'redux-thunk'
+
 const devTools = window.__REDUX_DEVTOOLS_EXTENSION__
     && window.__REDUX_DEVTOOLS_EXTENSION__()
 
-const store = applyMiddleware(promise)(createStore)(reducers, devTools)
+const store = applyMiddleware(multi, thunk, promise)(createStore)(reducers, devTools)
 
 ReactDOM.render(
     <Provider store={store}>
