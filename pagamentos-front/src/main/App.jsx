a6db9eb0d95d94c9c6afe6f264ce6a7caddb677d
lucasjunior24 <lukateku@gmail.com>
2021-02-18 18:59:11 -0300

    Atualizando navegação

--- pagamentos-front/src/main/App.jsx
+++ pagamentos-front/src/main/App.jsx
@@ -5,16 +5,15 @@ import '../common/template/dependencias'
 import Footer from '../common/template/Footer';
 import Header from '../common/template/Header'
 import SideBar from '../common/template/SideBar';
-import Routes from './routes'
 import Messages from '../common/msg/messages'
 
-function App() {
+function App(props) {
   return (
     <div className="wrapper">
       <Header />
       <SideBar />
       <div className='content-wrapper'>
-          <Routes />
+          {props.children}
       </div>
       <Footer />
       <Messages />
