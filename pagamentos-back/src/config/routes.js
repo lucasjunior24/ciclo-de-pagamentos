68eb3c9dc8470bfd906c274f6a675f01ca1f0880
lucasjunior24 <lukateku@gmail.com>
2021-02-20 14:29:52 -0300

    Atualizando rotas

--- pagamentos-back/src/config/routes.js
+++ pagamentos-back/src/config/routes.js
@@ -1,11 +1,30 @@
 const express = require('express')
+const auth = require('./auth')
 
 module.exports = function(server) {
-    // definnir URL base para todas as rotas
-    const router = express.Router()
-    server.use('/api', router)
+        /*
+         * Rotas Protegidas
+         */
+
+    const protectedApi = express.Router()
+    server.use('/api', protectedApi)
+
+    protectedApi.use(auth)
 
     // Rotas de ciclo de pagamento
     const BillingCycle = require('../api/billingCycleService')
-    BillingCycle.register(router, '/billingCycles')
+    BillingCycle.register(protectedApi, '/billingCycles')
+
+
+        /*
+         * Rotas Abertas
+         */
+
+    const openApi = express.Router()
+    server.use('/oapi', openApi)
+
+    const AuthServise = require('../api/user/authService')
+    openApi.post('/login', AuthServise.login)
+    openApi.post('/signup', AuthServise.signup)
+    openApi.post('/validateToken', AuthServise.validateToken)
 }
